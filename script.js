f8bef7e7304f8f468958b2cc8b62d2b44933c0bb
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2024-03-24 23:29:31 +0800

    Update content

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 document.getElementById("meowButton").addEventListener("click", function() {
-    alert("Meow Meow Meow Meow");
+  var audio = new Audio('sound.mp3');
+  audio.play();
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const container = document.querySelector('.container');
+
+  container.addEventListener('mousemove', function(e) {
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    sparkle.style.top = `${e.clientY}px`;
+    sparkle.style.left = `${e.clientX}px`;
+    container.appendChild(sparkle);
+    setTimeout(() => sparkle.remove(), 1000);
   });
-  
+});
